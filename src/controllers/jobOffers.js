53afd17ac81f380ec3560c681f743c159e48da10
Klejda Rrapaj <rrapaj41@gmail.com>
2024-03-09 19:20:04 +0100

    Aggiunta modifica riguardante l'utilizzo di return al posto di reply.send

--- src/controllers/jobOffers.js
+++ src/controllers/jobOffers.js
@@ -1,14 +1,20 @@
 import jobOfferService from "../services/jobOffer.js";
 
+/**
+ * @param {Object} options plugin options, refer to https://fastify.dev/docs/latest/Reference/Plugins/#plugin-options
+ */
+
 export default async function routes(fastify, options) {
 
-    fastify.get('/:id', async (request, reply) => {
+    fastify.get('/:id', async (request, options) => {
         const jobId = parseInt(request.params.id);
         const jobOffer = await jobOfferService.getJobOfferById(jobId);
 
-        if (!jobOffer) {
-            reply.code(404).send({ error: 'Job offer not found or expired' });
-        } else {
+        if (!jobOffer) 
+        {
+          return { statusCode: 404, error: 'Job offer not found or expired' };
+        } 
+        else {
             const { id, position, employer_name, summary, description } = jobOffer;
             return { id, position, employer_name, summary, description };
         }
@@ -22,7 +28,7 @@ export default async function routes(fastify, options) {
                 limit: { type: 'number', default: 5, description: 'Number of items per page' }
             },
         },
-        handler: async function (request, reply) {
+        handler: async function (request, options) {
           const { position, page, limit } = request.query;
 
           const { totalOffers, paginatedOffers } = await jobOfferService.searchJobOffers(position, page, limit);
@@ -54,19 +60,19 @@ export default async function routes(fastify, options) {
                 required: ['status'],
             },
         },
-        handler: async (request, reply) => {
+        handler: async (request, options) => {
             const jobId = request.params.id;
             const { status } = request.body;
 
             const jobOffer = await jobOfferService.getJobOfferToUpdate(jobId);
 
-            if (!jobOffer) {
-                reply.code(404).send({ error: 'Job offer not found' });
-                return;
+            if (!jobOffer) 
+            {
+              return { statusCode: 404, error: 'Job offer not found' };
             }
 
             jobOffer.state = status;
-            return jobOffer;
+            return {statusCode: 201, jobOffer};
         },
     });
 }
